265a58b3d22612491a0e805dbc22b35fe0b5df8c
Bernard Mordan <bernardmordan@gmail.com>
2015-06-15 10:08:24 +0100

    review created prototypes on Team and ScoreBoard

--- src/score-board.js
+++ src/score-board.js
@@ -2,11 +2,24 @@ var localforage = require('localforage')
 var _ = require('underscore')
 
 var ScoreBoard = function (initTeams, cb) {
-  if(_.uniq(initTeams).length !== initTeams.length) throw new Error('team names must be unique')
   var self = this
-  self.reset = function (cb) {localforage.clear(cb)}
-  self.teams = initTeams.slice()
-  init.call(self, initTeams, cb)
+  if(_.uniq(initTeams).length !== initTeams.length) throw new Error('team names must be unique')
+  if(!initTeams) {
+    this.getTeams(function (err, teams) {
+      if (err) return cb(err)
+      if (!teams) return cb(new Error('No teams passed and no teams in storeage'))
+      init.call(self, teams, cb) 
+    })
+  }else{
+    this.teams = initTeams.slice()
+    init.call(this, initTeams, cb)
+  }
+}
+ScoreBoard.prototype.reset = function (cb) {
+  localforage.clear(cb)
+}
+ScoreBoard.prototype.getTeams = function (cb) {
+  localforage.getItem('scoreboardteams', cb)
 }
 
 function init (teams, cb) {
@@ -15,75 +28,37 @@ function init (teams, cb) {
     return cb(null, self)
   }
   var team = teams.pop()
-  self[team] = new Interface(team)
+  self[team] = new Team(team)
   init.call(self, teams, cb)
 }
 
 module.exports = ScoreBoard
 
-function Interface (team) {
-  this.team = function () {
-    return team
-  }
-  return this
+function Team (team) {
+  this.name = team
 }
-Interface.prototype.getScore = function (cb) {
-  var teamKey = this.team()
-  localforage.getItem(teamKey+'score', cb)
+Team.prototype.getPlayers = function (cb) {
+  localforage.getItem(this.name+'players', cb)
 }
-Interface.prototype.addScore = function (score, cb) {
-
-  var teamKey = this.team()
-
-  localforage.getItem(teamKey+'score', function (err, total) {
-    if (err) throw new Error('Boo can\'t recover score')
-    if (!total) return localforage.setItem(teamKey+'score', score, cb)
-    var newScore = parseInt(total) + parseInt(score)
-    localforage.setItem(teamKey+'score', newScore, cb)
-  })
+Team.prototype.setPlayers = function (players, cb) {
+  localforage.setItem(this.name+'players', players, cb)
 }
-Interface.prototype.deduct = function (score, cb) {
-
-  var teamKey = this.team()
-
-  localforage.getItem(teamKey+'score', function (err, total) {
-    if (err) throw new Error('Boo can\'t recover score')
-    var newScore = parseInt(total) - parseInt(score)
-    localforage.setItem(teamKey+'score', newScore, cb)
-  })
+Team.prototype.getScore = function (cb) {
+  localforage.getItem(this.name + 'score', cb)
 }
-Interface.prototype.addPlayer = function (role, name, cb) {
-  var teamKey = this.team() + 'players'
-  this[role] = function (cb) {
-    localforage.getItem(teamKey, function (err, players) {
-      if (err) throw new Error(err)
-      if (!players) {
-        var players = []
-        storeNewPlayer(teamKey, players, role, name, cb)
-      }else{
-        updatePlayer(teamKey, players, role, name, cb)
-      }
-    })
-  }
-  cb(null, name)
+Team.prototype.addScore = function (score, cb) {
+  localforage.getItem(this.name + 'score', function (err, total) {
+    if (err) return cb(err)
+    total = total || 0
+    var newScore = total + parseInt(score)
+    localforage.setItem(this.name + 'score', newScore, cb)
+  }.bind(this))
 }
-
-function Player (role, name) {
-  return this[role] = name
-}
-
-function storeNewPlayer (teamKey, players, role, name, cb) {
-  players.push(new Player(role, name))
-  storePlayer(teamKey, players, role, cb)
-}
-function updatePlayer (teamKey, players, role, name, cb) {
-  var index = _.findIndex(players, function (player) { return _.keys(player)[0] == role })
-  players[index] = new Player(role, name)
-  storePlayer(teamKey, players, role, cb)
-}
-function storePlayer (teamKey, players, role, cb) {
-  localforage.setItem(teamKey, players, function (err, players) {
-    var player = _.chain(players).pluck(role).first().value()
-    cb(null, player)    
-  })
+Team.prototype.deductScore = function (score, cb) {
+  localforage.getItem(this.name + 'score', function (err, total) {
+    if (err) return cb(err)
+    total = total || 0
+    var newScore = total - parseInt(score)
+    localforage.setItem(this.name + 'score', newScore, cb)
+  }.bind(this))
 }
